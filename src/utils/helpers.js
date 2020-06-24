a38eafa6f50a83d1ad3e2cfae42554fa30659283
seifElislam <seif.eleslam1990@gmail.com>
2020-06-24 03:32:17 +0200

    style: add buttons colors

---
+++ src/utils/helpers.js
@@ -0,0 +1,26 @@
+export const colors = {
+    green: {
+      name: 'green',
+      hex: '#7BB31A'
+    },
+    blue: {
+      name: 'blue',
+      hex: '#66CCFF'
+    },
+    grey: {
+      name: null,
+      hex: '#CCCCCC'
+    }
+  };
+  
+  export const styles = {
+    primary: {
+      color: 'green',
+      bgColor: 'honeydew'
+    },
+    secondary: {
+      color: 'grey',
+      bgColor: '#f4f4f4'
+    }
+  };
+  
