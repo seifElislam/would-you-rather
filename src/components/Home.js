0083c9f002bfe0158fe51ed359e46fd8e9a9472c
seifElislam <seif.eleslam1990@gmail.com>
2020-06-24 03:14:48 +0200

    feat: add home component

---
+++ src/components/Home.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Tab } from 'semantic-ui-react';
+
+export class Home extends Component {
+  static propTypes = {
+    userQuestionData: PropTypes.object.isRequired
+  };
+  render() {
+    const { userQuestionData } = this.props;
+
+    return <Tab panes={panes({ userQuestionData })} className="tab" />;
+  }
+}
+
+const panes = props => {
+  const { userQuestionData } = props;
+  return [
+    {
+      menuItem: 'Unanswered',
+      render: () => (
+        <Tab.Pane>
+         tab1
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: 'Answered',
+      render: () => (
+        <Tab.Pane>
+          tab2
+        </Tab.Pane>
+      )
+    }
+  ];
+};
+
+function mapStateToProps({ authUser, users, questions }) {
+  const answeredIds = Object.keys(users[authUser].answers);
+  const answered = Object.values(questions)
+    .filter(question => !answeredIds.includes(question.id))
+    .sort((a, b) => b.timestamp - a.timestamp);
+  const unanswered = Object.values(questions)
+    .filter(question => answeredIds.includes(question.id))
+    .sort((a, b) => b.timestamp - a.timestamp);
+
+  return {
+    userQuestionData: {
+      answered,
+      unanswered
+    }
+  };
+}
+
+export default connect(mapStateToProps)(Home);
