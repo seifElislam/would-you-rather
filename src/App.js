484304db6a7a0539db24686ef3a4774843f8294c
seifElislam <seif.eleslam1990@gmail.com>
2020-06-24 03:16:01 +0200

    feat: add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
+import { handleInitialData } from './actions/shared';
+import { connect } from 'react-redux';
 import './App.css';
+import Login from './components/Login';
+import Home from './components/Home';
+import Nav from './components/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData();
+  }
+  render() {
+    const { authUser } = this.props;
+    return (
+      <Router>
+        <div className="App">
+          {authUser === null ? (
+            <Route
+              render={() => (
+                <ContentGrid>
+                <Login />
+                </ContentGrid>
+              )}
+            />
+          ) : (
+              <Fragment>
+                <Nav />
+                <ContentGrid>
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                  </Switch>
+                </ContentGrid>
+              </Fragment>
+            )}
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const ContentGrid = ({ children }) => (
+  <Grid padded="vertically" columns={1} centered>
+    <Grid.Row>
+      <Grid.Column style={{ maxWidth: 550 }}>{children}</Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+function mapStateToProps({ authUser }) {
+  return {
+    authUser
+  };
+}
+
+export default connect(mapStateToProps, { handleInitialData })(App);
